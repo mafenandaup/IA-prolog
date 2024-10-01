883cbb86c4ec5f0b4d839d65c1a91d467b8c189c
Maria Fernanda Maia <mafenandaup@gmail.com>
2024-09-30 21:05:53 -0300

    k

---
+++ hi.pl
@@ -0,0 +1,8 @@
+Nome = "fefis" 
+% essa é uma variável nome
+'Babel','hitchcock','16'
+% essas são sequencias de átomos, 
+gosta(joao, pizza).
+gosta(maria, sorvete).
+gosta(ana, pizza).
+amigo(X, Y) :- gosta(X, Z), gosta(Y, Z).
