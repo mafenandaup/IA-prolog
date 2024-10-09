7c406f7f0e983baa3c1d6cfc490026602ceb3c18
Maria Fernanda Maia <mafenandaup@gmail.com>
2024-10-09 19:31:32 -0300

    kk

--- newthing.pl
+++ newthing.pl
@@ -1,5 +1,8 @@
-pessoa(maria).
-pessoa(joao).
-
-quer_pizza(X) :- X = maria.
-?- quer_pizza(joao).
+filho(joao,ana).
+filho(ana,maria).
+filho(maria,pedro).
+filho(gabriel,pedro).
+filho(livia,gabriel).
+filho(livia,carla).
+descendente(X,Y):-filho(X,Y).
+descendente(X,Z):-filho(X,Y),descendente(Y,Z).
