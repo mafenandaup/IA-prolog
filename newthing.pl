76f61a57ef95f21a3d4e0d703b0065fcdba9bc78
Maria Fernanda Maia <mafenandaup@gmail.com>
2024-10-09 20:18:12 -0300

    k

--- newthing.pl
+++ newthing.pl
@@ -1,8 +1,5 @@
-filho(joao,ana).
-filho(ana,maria).
-filho(maria,pedro).
-filho(gabriel,pedro).
-filho(livia,gabriel).
-filho(livia,carla).
-descendente(X,Y):-filho(X,Y).
-descendente(X,Z):-filho(X,Y),descendente(Y,Z).
+fruta(manga).
+fruta(banana).
+fruta(limao).
+doce(X) :- fruta(X), (X = manga; X = banana).
+?- doce(limao).
