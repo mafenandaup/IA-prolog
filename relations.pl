b457a6df469619f6f20972c9cdef12949b2e3808
Maria Fernanda Maia <mafenandaup@gmail.com>
2024-10-14 17:44:22 -0300

    k

--- relations.pl
+++ relations.pl
@@ -3,4 +3,10 @@
 % CABECA: AMNÉSIA, CAUDA DA LISTA; RESTO DOS ELEMENTOS. a lista, nesse caso, tem 
 %tamanho 4
 
+soma_lista([], 0).
+soma_lista([H|T], Soma) :-
+    soma_lista(T, SomaT),
+    Soma is H + SomaT.
 
+divisao_lista([],0).
+divisao_lista
