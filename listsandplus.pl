062567950596fc5ab05634e35b26be9fc60769ff
Maria Fernanda Maia <mafenandaup@gmail.com>
2024-10-15 19:26:54 -0300

    k

--- listsandplus.pl
+++ listsandplus.pl
@@ -4,4 +4,5 @@ meusfavs(['danca','canto','pintura','bailes','desenhos']).
 %cabeça: dança,; cauda: resto.
 listavazia(['oi']). %%pra executar: listavazia([H|T]) = listavazia(['oi']).
 
-pairparts([H|T],)
+pairparts([H|T], H,T).
+%9 ?- pairparts(a,b,c). é falso porque os itens abaixo não são pares ou listas.
