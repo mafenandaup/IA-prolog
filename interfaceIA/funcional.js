8c31f982a43d4a2ac241fb07f079684216fb376c
Maria Fernanda Maia <mafenandaup@gmail.com>
2024-10-17 16:48:55 -0300

    kk

--- interfaceIA/funcional.js
+++ interfaceIA/funcional.js
@@ -12,12 +12,24 @@ document.getElementById('diagnosis-form').addEventListener('submit', function(ev
     } else if (symptoms.includes('espirro') && symptoms.includes('nariz entupido')) {
         diagnosis = 'Resfriado';
         treatment = 'Hidratação';
+    } else if (symptoms.includes('') ) {
+        diagnosis = '';
+        treatment = '';
+    } else if (symptoms.includes('') ) {
+        diagnosis = '';
+        treatment = '';
+    } else if (symptoms.includes('') ) {
+        diagnosis = '';
+        treatment = '';
+    } else if (symptoms.includes('') ) {
+        diagnosis = '';
+        treatment = '';
     } else if (symptoms.includes('febre') && symptoms.includes('falta de ar') && symptoms.includes('dor de garganta'),symptoms.includes('sem paladar')) {
         diagnosis = 'Covid-19';
         treatment = 'Isolamento, Hidratação frequente, antitérmicos(controle da febre), Higiene frequente e uso de máscaras respiratórias.';
     } else {
         diagnosis = 'Sintomas desconhecidos';
-        treatment = 'Consulte um médico';
+        treatment = 'Caso não tenha-o feito, consulte um médico.';
     }
 
     document.getElementById('result').innerHTML = `<h2>Diagnóstico: ${diagnosis}</h2><p>Tratamento recomendado: ${treatment}</p>`;
